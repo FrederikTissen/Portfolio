4ede0d897348a4159a77367217c953d317dac2f0
FrederikTissen <117523130+FrederikTissen@users.noreply.github.com>
2023-06-09 14:28:33 +0200

    bug animatione

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { StartscreenComponent } from './startscreen/startscreen.component';
 
+
+
+
 const routes: Routes = [
 
 ];
