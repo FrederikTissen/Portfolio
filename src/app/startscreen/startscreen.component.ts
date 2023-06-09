e7bc1b1d9f70c0da915f0bb679030e6f8b93aa07
FrederikTissen <117523130+FrederikTissen@users.noreply.github.com>
2023-06-09 11:24:17 +0200

    .

--- src/app/startscreen/startscreen.component.ts
+++ src/app/startscreen/startscreen.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-startscreen',
   templateUrl: './startscreen.component.html',
   styleUrls: ['./startscreen.component.scss']
 })
-export class StartscreenComponent {
+export class StartscreenComponent implements OnInit {
 
-}
+
+    images = ['arrow-down1.png', 'arrow-down3.png', 'arrow-down4.png', 'arrow-down5.png'];
+    currentImage = 0;
+    showImage = true;
+  
+    ngOnInit() {
+
+    }
+  
+  
+    updateImage() {
+      setInterval(() => {
+        this.currentImage++;
+        this.currentImage = this.currentImage % this.images.length;
+        /*this.showImage = false;
+        setTimeout(() => {
+          this.showImage = true;
+        }, 10);*/
+      }, 1000);
+    }
+  }
+
+
